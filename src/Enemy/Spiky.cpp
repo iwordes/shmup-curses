18e3fa7fb143bd6450ad5bcdad94c32642847852
Ian Wordes <iwordes@student.42.us.org>
2017-07-09 21:39:08 -0700

    Add Spiky

---
+++ src/Enemy/Spiky.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Spiky.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/09 20:50:31 by iwordes           #+#    #+#             */
+/*   Updated: 2017/07/09 21:37:50 by iwordes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <EnemySpiky.hpp>
+
+EnemySpiky::EnemySpiky(int16_t x, int16_t y): Enemy("X", x, y, 1, 1)
+{
+	this->maxTtFire = 8;
+	this->maxTtMove = 1;
+	this->ttFire = maxTtFire;
+	this->ttMove = maxTtMove;
+	this->ff = false;
+}
+
+EnemySpiky::EnemySpiky(const EnemySpiky &) {}
+EnemySpiky::~EnemySpiky() {}
+
+EnemySpiky &EnemySpiky::operator=(const EnemySpiky &) { return *this; }
+
+void EnemySpiky::onFire(World &world)
+{
+	world.addFg(new Projectile(type, "\\", x - 1, y - 1, -3, -3)); // NW
+	world.addFg(new Projectile(type, "/", x + 1, y - 1, 3, -3));   // NE
+	world.addFg(new Projectile(type, "\\", x + 1, y + 1, 3, 3));   // SE
+	world.addFg(new Projectile(type, "/", x - 1, y + 1, -3, 3));   // SW
+	world.addFg(new Projectile(type, "|", x, y - 1, 0, -3));       // N
+	world.addFg(new Projectile(type, "-", x + 1, y, 3, 0));        // E
+	world.addFg(new Projectile(type, "|", x, y + 1, 0, 3));        // S
+	world.addFg(new Projectile(type, "-", x - 1, y, -3, 0));       // W
+}
--- src/World/spawn.cpp
+++ src/World/spawn.cpp
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 12:11:07 by iwordes           #+#    #+#             */
-/*   Updated: 2017/07/09 21:17:16 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/07/09 21:38:37 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,40 @@
 
 #include <EnemyGatling.hpp>
 #include <EnemyTwirly.hpp>
+#include <EnemySpiky.hpp>
 
 static std::random_device rd;
 static std::mt19937 rng(rd());
 
-/*
 static bool chance(int n, int max)
 {
 	return (rng() % max < (unsigned)n);
 }
-*/
 
 #define ADD(T) addFg(new T(w - 1, rng() % h))
 
 void World::spawn()
 {
+	if (chance(1, 10))
+		ADD(EnemySpiky);
+		return;
+
 	if (wave >= 15 && chance(1, 8))
-		ADD(EnemyTwirly);
+		;// ...
 	else if (wave >= 15 && chance(1, 30))
 		ADD(EnemyGatling);
-	else if (wave >= 10 && chance(1, 8))
-		ADD(EnemyOcto);
 	else if (wave >= 10 && chance(1, 5))
-		;// ...
+		ADD(EnemyTwirly);
 	else if (wave >= 5 && chance(1, 3))
-		;// ...
+	{
+		if (wave >= 10)
+			ADD(EnemySpiky);
+		else
+			ADD(EnemyOcto);
+	}
 	else if (wave >= 5 && chance(1, 3))
+		;//ADD(EnemyShotty);
+	else if (wave >= 3 && chance(1, 3))
 		ADD(EnemyBomber);
 	else if (chance(1, 3))
 		ADD(EnemyShooter);
